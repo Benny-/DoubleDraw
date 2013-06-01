7be098132dbf1666b756e8279dd1b3285b174ef2
OpenShift guest <benny@gmx.it>
2013-06-01 13:17:25 -0400

    Gave up on using apf toolkit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
+  , paper = require('paper')  // Note: There are two paperjs variants.
+                              // One for the browser and one for nodejs.
+                              // Functionality they are the same.
+                              // The one for the browser is in ./Client
+                              // The one for nodejs is located in ./node_modules
+
+io.set('loglevel',0)
+
+app.use(express.compress());
+app.use(express.static(__dirname + '/client'));
+server.listen(process.env.PORT);
+
+io.sockets.on('connection', function (socket) {
+    console.log("New socket.io connection. More friends! yay");
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
